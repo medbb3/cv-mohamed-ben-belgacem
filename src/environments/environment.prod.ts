a2cc4ed0fd673d7c1ebc814c7eff9cbb65c850a9
medbb <med.benbelgacem@gmail.com>
2020-12-31 09:55:50 +0100

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDWYEGCve_iQmHb2Alz72CVsSyP8EzgE_M",
+    authDomain: "cv-medbb.firebaseapp.com",
+    projectId: "cv-medbb",
+    storageBucket: "cv-medbb.appspot.com",
+    messagingSenderId: "596071415175",
+    appId: "1:596071415175:web:15d9f4621b7a5f0dea2722",
+    measurementId: "G-G8PE40JKBG"
+  }
+};
